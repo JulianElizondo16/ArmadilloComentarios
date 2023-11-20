bc03b1a4c09108d19f8b459ecae24f09c0948add
Julian Elizondo Cerda <JulianECPersonal@gmail.com>
2023-11-20 08:53:04 -0600

    Solamente los comentarios

--- src/ComentariosServiceProvider.php
+++ src/ComentariosServiceProvider.php
@@ -20,26 +20,29 @@ class ComentariosServiceProvider extends ServiceProvider
     public function boot(): void
     {
 
-       /*  Base de datos */
-       $this->publishes([
-        __DIR__.'/../database/migrations' => database_path('migrations'),
-    ], 'migrations');
-        
-   /*  Llamada de vistas */
+        /*  Base de datos */
         $this->publishes([
-            __DIR__.'/views/vistasPrincipales' => resource_path('views/vistasPrincipales'),
+            __DIR__ . '/../database/migrations' => database_path('migrations'),
+        ], 'migrations');
+
+        /*  Llamada de vistas */
+        $this->publishes([
+            __DIR__ . '/views/vistasPrincipales' => resource_path('views/vistasPrincipales'),
         ], 'VistasComentarios');
         $this->publishes([
-            __DIR__.'/resources/views/livewire' => resource_path('views/livewire'),
+            __DIR__ . '/resources/views/livewire' => resource_path('views/livewire'),
         ], 'navigation');
+        /* Ingresar las imagenes del paquete */
+        $this->publishes([
+            __DIR__ . '/public/img' => public_path('img'),
+        ], 'ImagenesCyber');
         /* Llamada de controladores */
         $this->publishes([
-            __DIR__.'/Controllers' => app_path('/Http/Controllers'),
+            __DIR__ . '/Controllers' => app_path('/Http/Controllers'),
         ], 'ComentariosController');
         /* Llamada de los Modelos */
         $this->publishes([
             __DIR__ . '/Models' => app_path('Models'),
         ], 'Comentario');
-
     }
 }
